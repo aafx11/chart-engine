16cae3da4c1551f37d43d1c232acfb031f507a09
fx <903921845@qq.com>
2023-05-11 21:52:21 +0800

    chore: 删除测试文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,15 @@ module.exports = {
     rules: {
         'linebreak-style': ['error', 'windows'],
         'import/no-unresolved': 0,
-        'no-unused-vars': 0
+        'no-unused-vars': 0,
+        'import/prefer-default-export': 0,
+        'no-use-before-define': 0,
+        'no-shadow': 0,
+        'no-restricted-syntax': 0,
+        'no-return-assign': 0,
+        'no-param-reassign': 0,
+        'no-sequences': 0,
+        'no-loop-func': 0,
+        'no-nested-ternary': 0,
     }
 }
